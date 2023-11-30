a1b3146c840156af3ed5e4d67be3b539dc8dfdce
shoaibahmed1085 <71823028+shoaibahmed1085@users.noreply.github.com>
2023-11-30 12:44:34 +0530

    Update clk_div_by_2N.sv
    Replaced * operator with << operator to reduce hardware usage

--- clk_div_by_2N.sv
+++ clk_div_by_2N.sv
@@ -2,28 +2,27 @@
 //output should be high for N full clk cycles and then be toggled
 
 module clk_div_2N #(
-  	//parameter WIDTH = 3,
 	parameter N=4
 ) (
 	input i_clk, rst,
   	output reg out_clk
 );
   
-  reg [2*N-1:0] count;
+  reg [(N<<1)-1:0] count;
   
   always @(posedge i_clk) begin
     if(rst)
       out_clk <= 'b0;
     else if (count<=N-1)
       out_clk <= 'b1;
-    else if(count>=N && count<=2*N-1)
+    else if(count>=N && (count <= (N<<1)-1) )
       out_clk <= 'b0;
   end
   
   always @(posedge i_clk) begin
     if(rst)
       count = 'b0;
-    else if(count==2*N-1)
+    else if(count==(N<<1)-1)
       count = 'b0;
     else
       count = count + 1'b1;
